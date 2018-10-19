8a49afe349a87ed63c82fea063858755b1b41acc
AlessiaYChen <42006584+AlessiaYChen@users.noreply.github.com>
2018-10-19 11:51:12 -0700

    update accordions

--- assets/ik_accordion.js
+++ assets/ik_accordion.js
@@ -38,7 +38,9 @@
 		}).addClass('ik_accordion');
 		
 		$elem.attr({'aria-multiselectable': !this.options.autoCollapse}); // define if more than one panel can be expanded
-			
+		
+		this.headers = $elem.children('dt').attr({'role': 'heading'}); // set heading role for each accordion header
+		
 		this.headers = $elem.children('dt').each(function(i, el) {
 			var $me, $btn;
 			
